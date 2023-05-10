8c22edab798cb83c509522e73ed79d890e7d7a3d
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-05-11 03:35:23 +0530

    Backend routing changes and corrections

--- index.js
+++ index.js
@@ -96,6 +96,7 @@ server.use('/users', isAuth(), usersRouter.router);
 server.use('/auth', authRouter.router);
 server.use('/cart', isAuth(), cartRouter.router);
 server.use('/orders', isAuth(), ordersRouter.router);
+// this line we add to make react router work in case of other routes doesnt match
 server.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
 
 // Passport Strategies
@@ -174,7 +175,7 @@ const stripe = require("stripe")(process.env.STRIPE_SERVER_KEY);
 
 
 server.post("/create-payment-intent", async (req, res) => {
-  const { totalAmount } = req.body;
+  const { totalAmount, orderId } = req.body;
 
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -183,6 +184,9 @@ server.post("/create-payment-intent", async (req, res) => {
     automatic_payment_methods: {
       enabled: true,
     },
+    metadata:{
+        orderId
+      }
   });
 
   res.send({
